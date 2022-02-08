c16c3cde21eedb83af4c76013fce1452ec210cf0
Catherine Armstrong <c.armstrong@sportradar.com>
2022-02-08 09:04:48 -0600

    fix auth missing build error

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -18,8 +18,8 @@ const devConfig = {
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        auth: 'auth@http://localhost:8082/remoteEntry.js',
         marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+        auth: 'auth@http://localhost:8082/remoteEntry.js',
       },
       shared: packageJson.dependencies,
     }),
